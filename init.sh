0a00cd3624e861103dac2c53a86c3831956bd8a8
Hikmat Jafarli <jafarlihi@gmail.com>
2020-01-02 21:33:47 +0400

    Fix init.sh

--- init.sh
+++ init.sh
@@ -40,7 +40,7 @@ function create_test_file {
 function register_test_file {
     # TODO: Make this less hacky
     echo "#include <$1-test.h>" >> ./tests/main-test.c
-    echo "extern void test_NAMEOFUNIT_firstTest(void);" >> ./tests/main-test.c
+    echo "extern void test_$1_firstTest(void);" >> ./tests/main-test.c
 }
 
 function additional_module_prompt {
